85259fd671df8cb9b2f1903a921ccf4cf36de79c
Gintokyo <153867001+Gintokyo@users.noreply.github.com>
2025-08-25 13:26:45 +0100

    Create cards.py
    Cards

---
+++ cards.py
@@ -0,0 +1,34 @@
+# Cards
+# Damage cards
+class Card:
+    def __init__(self, name, effect, power):
+        self.name = name
+        self.effect = effect
+        self.power = power
+        
+    # Printing card
+    def __str__(self):
+        return f"{self.name} {self.effect} {self.power}"
+
+fireball = Card("Fireball", "attack", 5)
+dmg_deck = [
+    fireball
+]
+
+print(f"{fireball}")
+
+# Healing cards
+heal = Card("Heal", "Heal", 5)
+heal_deck = [
+    heal
+]
+
+print(f"{heal}")
+
+# Status cards
+poison = Card("Poison", "Poison damage", 1)
+status_deck = [
+    poison
+]
+
+print(f"{poison}")
