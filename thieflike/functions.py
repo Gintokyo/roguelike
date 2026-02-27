8304ca4ba0c362748eeba1e0ae4a519751791579
Gintokyo <153867001+Gintokyo@users.noreply.github.com>
2026-02-27 17:19:49 +0000

    Added functions to deal with the enemy_deck and to select a card from it

--- thieflike/functions.py
+++ thieflike/functions.py
@@ -1,7 +1,7 @@
 # Contains most of the functions
 
 # Importing the player_deck
-from cards import player_deck
+from cards import player_deck, enemy_deck
 from classes import enemy_list
 # This is needed to randomly select what enemy the player's gonna face at a time
 import random
@@ -30,12 +30,24 @@ def enemy_select():
     currentEnemy = random.choice(enemy_list)
     return currentEnemy
 
+# Function to print cards in enemy's deck (will become Cards in Hand)
+def opponent_deck():
+    n = 1
+    for card in enemy_deck:
+        print(f"{n}. {card}")
+        n += 1
+
+# The enemy selects a card
+def opponent_card():
+    enemy_card = random.choice(enemy_deck)
+    print(enemy_card)
+
 # Function to keep track of turns NOT READY YET
-def battle_turns(x, player, enemy):
-   turno = x
-   while player.stats["HP"] >0 and enemy.stats["HP"] > 0:
-       current_turn = turno
-       current_turn += 1
-       print(current_turn)
-   else:
-       gameOver = True
+#def battle_turns(x, player, enemy):
+#   turno = x
+#   while player.stats["HP"] >0 and enemy.stats["HP"] > 0:
+#       current_turn = turno
+#       current_turn += 1
+#       print(current_turn)
+#   else:
+#       gameOver = True
