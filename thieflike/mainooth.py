1058fff604c47c4bc37a38663769f9b3e49d9e74
Gintokyo <153867001+Gintokyo@users.noreply.github.com>
2026-03-03 16:55:18 +0000

    Update mainooth.py
    Smøl update to perform

--- thieflike/mainooth.py
+++ thieflike/mainooth.py
@@ -64,7 +64,7 @@ while gameOver == False:
         selected_card.applyEffect(selectedClasse, current_enemy)
         # TODO: make it apply the effect of the card and only play if the enemy is still alive
         opponent_choice = opponent_card()
-# This should be opponent_choice not selected_card
+        # This should be opponent_choice not selected_card
         selected_card.applyEffect(current_enemy, selectedClasse)
         #battle_turns(turn, selectedClasse, current_enemy)
         turn += 1
